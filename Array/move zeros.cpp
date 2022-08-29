fb2df57fba2fc975e5499b238c632655e1f13288
Saurav Tayade <90100594+sauravbot19@users.noreply.github.com>
2022-08-29 21:38:10 +0530

    Create move zeros.cpp

---
+++ Array/move zeros.cpp
@@ -0,0 +1,15 @@
+// move zeros leetcode solution
+// c++
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != 0) {
+                swap(nums[i], nums[j++]);
+            }
+        }
+    }
+};
