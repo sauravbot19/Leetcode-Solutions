bad7c624a247cfe94ac532b6d2e1df6920156123
Saurav Tayade <90100594+sauravbot19@users.noreply.github.com>
2022-08-30 21:48:05 +0530

    Create Squares of a sorted array.cpp

---
+++ Array/Squares of a sorted array.cpp
@@ -0,0 +1,15 @@
+// squares of a sorted array
+// c++
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        for(int i = 0; i < nums.size(); i++) {
+            nums[i] *= nums[i];
+        }
+        sort(nums.begin(), nums.end());
+        
+        return nums;
+    }
+};
